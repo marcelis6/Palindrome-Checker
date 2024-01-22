ed5c6fdb8ecd2dca1b5983d40bf1a7c2988e48a3
marcelis6 <123846226+marcelis6@users.noreply.github.com>
2024-01-22 13:58:58 +0100

    added checking on enter feature

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
- let text;
- let odp = document.getElementById("answer");
- 
+let text;
+let odp = document.getElementById("answer");
+const Button = document.getElementById("btn");
 
-document.getElementById("btn").addEventListener("click", function(){
-    text = document.getElementById("TXT").value;
-    TextReversal();
+Button.addEventListener("click", function(){
+   text = document.getElementById("TXT").value;
+   TextReversal();
 } );
+addEventListener("keypress", function(e){ 
+   if(e.key === "Enter") {
+       text = this.document.getElementById("TXT").value;
+       TextReversal();
+       console.log("enter pressed")
+   }
+})
 
- 
 function TextReversal(){
-     let text2 = text.split("").reverse().join("");
-    if (text == text2){
-        odp.innerHTML = "True";
-    } else odp.innerHTML = "False";
-    console.log(text);
-    console.log(text2);
+    let text2 = text.split("").reverse().join("");
+   if (text == text2){
+       odp.innerHTML = "This is a Palindrome :D";
+   } else odp.innerHTML = "That's not a Palindrome :(";
+   console.log(text);
+   console.log(text2);
 } 
 
